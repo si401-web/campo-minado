88a571b735d8f9072f2cc3ffd45e08568650cf92
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-10-16 17:04:17 -0300

    Added nova partida javascript

---
+++ js/nova_partida.js
@@ -0,0 +1,15 @@
+function criarPartida() {
+    var form = document.forms["partida"];
+
+    var colunas = form["colunas"].value;
+    var linhas = form["linhas"].value;
+    var bombas = form["bombas"].value;
+    var modalidade = form["modalidade"].value;
+
+    if (colunas * linhas < bombas) {
+        alert('Não é possível ter ' + bombas + ' bombas em um tabuleiro de ' + colunas + 'x' + linhas + '.');
+        return false;
+    }
+
+    return colunas != "" && linhas != "" && bombas != "" && modalidade != "";
+}
