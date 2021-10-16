88f1d9f030504e521d6de47fc84a46c89dfd12b1
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-10-16 20:23:55 -0300

    Added timer logic

--- js/jogo.js
+++ js/jogo.js
@@ -12,9 +12,14 @@ var board = [
 
 var exploded = false;
 var win = false;
+var timeout = false;
+
+function finished() {
+	return win || exploded || timeout;
+}
 
 function openBlock(line, column) {
-	if (exploded || win)
+	if (finished())
 		return;
 
 	var id = "cell-" + line + "-" + column;
@@ -38,7 +43,7 @@ function openBlock(line, column) {
 
 		toggleBombs("&#x1f4a3;");
 		exploded = true;
-		alert('Você perdeu...');
+		alert("Você perdeu...");
 
 	} else if (value == 0) {
 		openBlock(line-1, column-1);
@@ -58,17 +63,21 @@ function openBlock(line, column) {
 
 	if (win) {
 		toggleBombs("&#9873;");
-		alert('Você ganhou!');
+		alert("Você ganhou!");
 	}
 }
 
-function toggleBombs(icon) {
+function toggleBombs(icon, explode) {
 	for(var l = 0; l < board.length; l++) {
 		for(var c = 0; c < board[l].length; c++) {
 			if (board[l][c] == "B") {
 				var bombId = "cell-" + l + "-" + c;
 				var bombBlock = document.getElementById(bombId);
 				bombBlock.innerHTML = icon;
+
+				if (explode) {
+					bombBlock.classList.add("explodiu");
+				}
 			}
 		}
 	}
@@ -90,4 +99,58 @@ function checkWin() {
 	}
 
 	return true;
+}
+
+var spentTime = 0;
+var limitTime = 0;
+
+function timer() {
+	var params = new URLSearchParams(window.location.search);
+	if (params.get("modalidade") == "rivotril") {
+		document.getElementById("decorrido").classList.add("decorrido");
+		document.getElementById("restante").classList.add("restante");
+		document.getElementById("modalidade").innerHTML = "Modalidade Rivotril";
+
+		limitTime = 180;
+	}
+
+	adjustTimers();
+}
+
+function adjustTimers() {
+	adjustTimer("decorrido", spentTime);
+
+	if (limitTime > 0) {
+		adjustTimer("restante", limitTime - spentTime);
+
+		if (spentTime == limitTime) {
+			toggleBombs("&#x1f4a3;", true);
+			timeout = true;
+			alert("Você perdeu...");
+			return;
+		}
+	}
+
+	setTimeout(() => {
+		if (!finished()) {
+			spentTime++;
+			adjustTimers();
+		}
+	}, 1000)
+}
+
+function adjustTimer(id, number) {
+	var seconds = number % 60;
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+
+	var minutes = (number - seconds) / 60;
+	if (minutes < 10) {
+		minutes = "0" + minutes;
+	}
+
+	var element = document.getElementById(id);
+	element.innerHTML = "&nbsp;&nbsp;&nbsp;" + minutes + ":" + seconds + "&nbsp;&nbsp;&nbsp;";
+	element.style.width = number * 100 / limitTime + "%";
 }
