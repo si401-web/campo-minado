40d78bd0d08df52f1d562dffea337d22118bc920
Karlos Helton Braga <karlos.hbraga@gmail.com>
2021-10-16 18:21:29 -0300

    Added game logic

---
+++ js/jogo.js
@@ -0,0 +1,71 @@
+var board = [
+	[  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0  ],
+	[  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0 ,  0  ],
+	[  1 ,  2 ,  2 ,  1 ,  0 ,  0 ,  0 ,  0 ,  0  ],
+	[  2 , "B", "B",  1 ,  0 ,  0 ,  0 ,  0 ,  0  ],
+	[ "B",  5 ,  3 ,  2 ,  0 ,  0 ,  0 ,  0 ,  0  ],
+	[ "B",  3 , "B",  1 ,  0 ,  1 ,  2 ,  2 ,  1  ],
+	[  1 ,  3 ,  2 ,  2 ,  0 ,  1 , "B", "B",  2  ],
+	[  0 ,  1 , "B",  1 ,  0 ,  1 ,  4 , "B",  3  ],
+	[  0 ,  1 ,  1 ,  1 ,  0 ,  0 ,  2 , "B",  2  ],
+]
+
+var exploded = false;
+
+function openBlock(line, column) {
+	if (exploded)
+		return;
+
+	var id = "cell-" + line + "-" + column;
+	var block = document.getElementById(id);
+
+	if (!block)
+		return;
+
+	var value = board[line][column];
+
+	if (value == 9)
+		return;
+
+	block.classList.add("vazio")
+	board[line][column] = 9;
+
+	console.log(line, column, value);
+	if (value == "B") {
+		block.innerHTML = "&#x1f4a3;";
+		block.classList.add("explodiu")
+		toggleBombs("&#x1f4a3;");
+		exploded = true;
+	} else if (value == 0) {
+		openBlock(line-1, column-1);
+		openBlock(line-1, column);
+		openBlock(line-1, column+1);
+		openBlock(line, column-1);
+		openBlock(line, column+1);
+		openBlock(line+1, column-1);
+		openBlock(line+1, column);
+		openBlock(line+1, column+1);
+	} else {
+		block.innerHTML = value;
+		block.classList.add("casa" + value)
+	}
+
+}
+
+function toggleBombs(icon) {
+	for(var l = 0; l < board.length; l++) {
+		for(var c = 0; c < board[l].length; c++) {
+			if (board[l][c] == "B") {
+				var bombId = "cell-" + l + "-" + c;
+				var bombBlock = document.getElementById(bombId);
+				bombBlock.innerHTML = icon;
+			}
+		}
+	}
+
+	if (icon == "X") {
+		setTimeout(() => {
+			toggleBombs("")
+		}, 5000);
+	}
+}
