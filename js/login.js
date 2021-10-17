858bce9d67b34a2aab633f94702743a147fc6629
mateusy <promateusy@gmail.com>
2021-10-17 15:11:09 -0300

    Login JavaScript + adicionei 2 coisas no css main.

---
+++ js/login.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.nomeUsuario.value;
+    const password = loginForm.senha.value;
+
+    if (username === "usuario" && password === "senha") {
+        window.location.replace("nova_partida.html");
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
