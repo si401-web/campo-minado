c26ca4aa4ce8274162dd94123f98c7a326f4e77e
unknown <jvportuense@gmail.com>
2021-11-18 18:49:59 -0300

    Tratamento de erros e redirecionamento de telas

--- cadastro.php
+++ cadastro.php
@@ -2,16 +2,10 @@
 /* require "testar_logado.php"; */
 include('conexao.php');
 ?>
-
 <?php
 require "db_models.php";
-
-$title = "Cadastro";
 $css = "form.css";
-
-require "html_inicio.php"
 ?>
-
 <?php
     if(isset($_POST['SendCadastro'])){
         $name = $_POST['Nome'];
@@ -25,13 +19,17 @@ require "html_inicio.php"
         $query = $conn->query("INSERT INTO USER (NAME,BIRTHDAY,CPF,PHONE,EMAIL,USERNAME,PASSWORD) VALUES ('$name','$dataNascimento','$cpf','$telefone','$email','$usuario','$senha')");
 
         if($query){
+            header('location: login.php');
             echo "Cadastro Realizado com Sucesso";
         }
         else{
             echo "Não foi possivel cadastrar";
         }
     }
-
+?>
+<?php
+$title = "Cadastro";
+require "html_inicio.php"
 ?>
 <section>
         <form class="container" id="cadastro-form" method="POST">
@@ -40,37 +38,37 @@ require "html_inicio.php"
 
             <div class="cadastro-campo">
                 <label>Nome Completo</label>
-                <input type="text" name="Nome" placeholder="Ex: João da Silva" />
+                <input type="text" name="Nome" placeholder="Ex: João da Silva" required />
             </div>
 
             <div class="cadastro-campo">
                 <label>Data de Nascimento</label>
-                <input type="text" name="DataNascimento" placeholder="0000-00-00" />
+                <input type="text" name="DataNascimento" placeholder="0000-00-00" minlength="10" maxlength="10" required  />
             </div>
 
             <div class="cadastro-campo">
                 <label>CPF</label>
-                <input type="text" name="CPF" minlength="11" maxlength="11" placeholder="00000000000" />
+                <input type="text" name="CPF" minlength="11" maxlength="11" placeholder="00000000000" required />
             </div>
 
             <div class="cadastro-campo">
                 <label>Telefone</label>
-                <input type="text" name="Telefone" placeholder="(xx)xxxxx-xxxx" />
+                <input type="text" name="Telefone" placeholder="55912345678" minlength="11" maxlength="11" required/>
             </div>
 
             <div class="cadastro-campo">
                 <label>E-mail</label>
-                <input type="email" name="Email" placeholder="exemplo@dominio.com" />
+                <input type="email" name="Email" placeholder="exemplo@dominio.com" required />
             </div>
 
             <div class="cadastro-campo">
                 <label>Usuário</label>
-                <input type="text" name="Usuario" placeholder="Digite seu nome de usuário" />
+                <input type="text" name="Usuario" placeholder="Digite seu nome de usuário" required />
             </div>
 
             <div class="cadastro-campo campo-alerta">
                 <label>Senha*</label>
-                <input type="password" name="Senha" minlength="6" maxlength="12" placeholder="Digite sua senha" />
+                <input type="password" name="Senha" minlength="6" maxlength="12" placeholder="Digite sua senha" required />
                 <span>*Digite de 6 a 12 caracteres</span>
             </div>
 
